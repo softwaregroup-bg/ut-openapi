a6ce12d8529dd4fd0efa33a4dc55dc435452c75c
stamen.peev <stamen.peev@softwaregroup-bg.com>
2020-02-17 15:09:03 +0200

    fix: set default value if provided in schema

--- format/request.js
+++ format/request.js
@@ -4,15 +4,16 @@ module.exports = ({url, method, schemas}) => (msg = {}) => {
     const {params = msg, body} = msg;
     const requestObject = {url, method, body, headers: {}, qs: {}};
     schemas.forEach(schema => {
+        const param = typeof params[schema.name] === 'undefined' ? schema.default : params[schema.name];
         switch (schema.in) {
             case 'header':
-                requestObject.headers[schema.name] = params[schema.name];
+                requestObject.headers[schema.name] = param;
                 break;
             case 'query':
-                requestObject.qs[schema.name] = params[schema.name];
+                requestObject.qs[schema.name] = param;
                 break;
             case 'path':
-                requestObject.url = interpolate(requestObject.url, {[schema.name]: params[schema.name]});
+                requestObject.url = interpolate(requestObject.url, {[schema.name]: param});
                 break;
             default:
                 break;

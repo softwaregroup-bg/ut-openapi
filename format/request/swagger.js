53a44aa828157876c1e6e063726c689ad59cd43e
stamen.peev <stamen.peev@softwaregroup-bg.com>
2020-02-21 15:36:37 +0200

    fix: don't set empty objects if the respective parameters are not described

--- format/request/swagger.js
+++ format/request/swagger.js
@@ -1,18 +1,20 @@
 const interpolate = require('ut-function.interpolate');
-
 module.exports = ({url, method, schemas}) => (msg = {}) => {
     const {params = msg, body} = msg;
-    const requestObject = {url, method, body, headers: {}, qs: {}, formData: {}};
+    const requestObject = {url, method, body};
     schemas.forEach(schema => {
         const param = typeof params[schema.name] === 'undefined' ? schema.default : params[schema.name];
         switch (schema.in) {
             case 'header':
+                if (!requestObject.headers) requestObject.headers = {};
                 requestObject.headers[schema.name] = param;
                 break;
             case 'query':
+                if (!requestObject.qs) requestObject.qs = {};
                 requestObject.qs[schema.name] = param;
                 break;
             case 'formData':
+                if (!requestObject.formData) requestObject.formData = {};
                 requestObject.formData[schema.name] = param;
                 break;
             case 'path':

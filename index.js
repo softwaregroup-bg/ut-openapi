4272ac2c30df004bf2f8b42fa7fa20e6b73ba713
Evgeni Atanasov <evgeni.atanasov@softwaregroup-bg.com>
2020-07-31 10:52:24 +0300

    feat: allow load of apis from multiple locations. allow config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const swaggerParser = require('@apidevtools/swagger-parser');
 const http = require('http');
+const merge = require('ut-function.merge');
 const httpVerbs = ['post', 'put', 'patch', 'get', 'delete', 'options', 'head', 'trace'];
 const formatters = {
     request: {
@@ -20,27 +21,29 @@ module.exports = () => {
         async load(namespace, document) {
             const docs = typeof namespace === 'object' ? Object.entries(namespace) : [[namespace, document]];
             for (let i = 0, n = docs.length; i < n; i += 1) {
-                const [ns, location] = docs[i];
-                let doc;
-                if (typeof location === 'object') doc = location;
-                else if (typeof location === 'string') {
-                    if (location.startsWith('http')) {
-                        doc = await new Promise((resolve, reject) => {
-                            http.get(location, res => {
-                                let data = '';
-                                res.on('data', chunk => { data += chunk; });
-                                res.on('end', () => {
-                                    try {
-                                        resolve(JSON.parse(data));
-                                    } catch (e) {
-                                        reject(e);
-                                    }
-                                });
-                            }).on('error', reject);
-                        });
-                    } else doc = require('./serverRequire')(location);
+                const [ns, locations] = docs[i];
+                let doc = [];
+                for (const location of [].concat(locations)) {
+                    if (typeof location === 'object') doc.push(location);
+                    else if (typeof location === 'string') {
+                        if (location.startsWith('http')) {
+                            doc.push(await new Promise((resolve, reject) => {
+                                http.get(location, res => {
+                                    let data = '';
+                                    res.on('data', chunk => { data += chunk; });
+                                    res.on('end', () => {
+                                        try {
+                                            resolve(JSON.parse(data));
+                                        } catch (e) {
+                                            reject(e);
+                                        }
+                                    });
+                                }).on('error', reject);
+                            }));
+                        } else doc.push(require('./serverRequire')(location));
+                    }
                 }
-                doc = await swaggerParser.bundle(doc);
+                doc = await swaggerParser.bundle(merge(...doc));
                 await swaggerParser.validate(doc);
                 const docType = doc.swagger ? 'swagger' : 'openapi';
                 Object
